890626d3db892b3a0c3298113af3c10c8fcb1663
antartsan <59975312+antartsan@users.noreply.github.com>
2020-01-16 13:39:33 -0600

    Added partner name and major
    I added two additional lines to display my name and major and my partners.

--- hello.c
+++ hello.c
@@ -1,4 +1,8 @@
+#include <stdio.h>
 int main(void){
 	printf("Diane Huynh\n");
 	printf("Computer Engineering\n");
+	printf("Antonio Sanchez\n");
+	printf("Electrical Engineering\n");
 	return 0;
+}
